2ed67655a95e23cfc680432cc71e0f0879c30756
auvmacintosh <120029790@qq.com>
2018-01-18 21:54:59 +0800

    second

--- my/bl.sh
+++ my/bl.sh
@@ -1,10 +1,16 @@
 while IFS= read -r line
 do
-	match=`echo "$line" | grep "0\."`
-	echo "$match"
-	if [ "$match" != null ]; then
-		echo "$line"haha
-	else
-		echo "$line"
-	fi
+	#match=`echo "$line" | grep "0\."`
+	#echo "$match"
+	#if [ "$match" != null ]; then
+	
+	#if `echo "$line" | grep "0\." >/dev/null 2>&1`; then
+	#	echo "$line"haha
+	#else
+	#	echo "$line"
+	#fi
+	case "$line" in
+		*"0."* ) echo haha;;
+		* ) echo "$line";;
+	esac
 done < "${1:-/dev/stdin}"
