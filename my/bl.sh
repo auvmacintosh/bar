714d95e0828f85650c316e3c72db99ed4ee64efe
auvmacintosh <120029790@qq.com>
2018-01-19 17:35:38 +0800

    finish bar.sh

--- my/bl.sh
+++ my/bl.sh
@@ -1,16 +1,21 @@
-while IFS= read -r line
+bar() {
+	echo $1
+}
+
+log() {
+	echo $1
+}
+
+while ifs= read -r line
 do
-	#match=`echo "$line" | grep "0\."`
-	#echo "$match"
-	#if [ "$match" != null ]; then
-	
-	#if `echo "$line" | grep "0\." >/dev/null 2>&1`; then
-	#	echo "$line"haha
-	#else
-	#	echo "$line"
-	#fi
 	case "$line" in
-		*"0."* ) echo haha;;
-		* ) echo "$line";;
+		*" 0."* ) #这块不行，最后会输出1
+			bar "$line"
+			;;
+		* ) 
+			log "$line"
+			;;
 	esac
 done < "${1:-/dev/stdin}"
+
+
